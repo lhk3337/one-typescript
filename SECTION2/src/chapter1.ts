44fe663263c848abfda690edd246849c1a9fc39a
lhk3337 <lhk3337@naver.com>
2023-05-23 21:48:09 +0900

    [SECTION2] 원시타입과 리터럴타입

---
+++ SECTION2/src/chapter1.ts
@@ -0,0 +1,46 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// num1 = "123"; type error
+// num1.toUpperCase();  string methord error
+
+// string
+
+let str1: string = "hello";
+let str2: string = "hello";
+let str3: string = "hello";
+let str4: string = "hello";
+let str5: string = "hello";
+
+// str = 1; type error
+// str1.toFixed(); number method error
+
+// boolean
+
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+
+let null1: null = null;
+
+// undefined
+
+let unde1: undefined = undefined;
+
+// 리터럴 타입
+
+let numA: 10 = 10;
+// numA = 12; // number 10 만 허용
+
+let strA: "hello" = "hello";
+// strA = "h"; // string "hello" 만 허용
+
+let boolA: true = true;
+// boolA = false; // boolean true만 허용

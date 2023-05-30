2c0b14b03d970d444e1741cc243d74abb2dc24ed
lhk3337 <lhk3337@naver.com>
2023-05-30 22:37:50 +0900

    [SECTION3] 객체 타입의 호환

---
+++ SECTION3/src/chapter3.ts
@@ -0,0 +1,65 @@
+let num1: number = 1;
+let num2: 10 = 10;
+num1 = num2;
+
+/**
+ ** 객체 타입간의 호환성
+ **어떤 객체타입을 다른 객체타입으로 취급해도 괜찮은가?
+ */
+
+type Animal = {
+  name: string;
+  color: string;
+};
+
+type Dog = {
+  name: string;
+  color: string;
+  breed: string;
+};
+
+let animal: Animal = {
+  name: "기린",
+  color: "yellow",
+};
+
+let dog: Dog = {
+  name: "바둑이",
+  color: "brown",
+  breed: "시고르자브종",
+};
+
+animal = dog;
+// dog = animal;
+
+// 객체의 프로퍼티가 적은 쪽이 슈퍼 타입
+
+type Book = {
+  name: string;
+  price: number;
+};
+
+type ProgrammingBook = {
+  name: string;
+  price: number;
+  skill: string;
+};
+
+let book: Book;
+
+let programmingBook: ProgrammingBook = {
+  name: "한 입 크기로 잘라먹는 리액트",
+  price: 33000,
+  skill: "reactjs",
+};
+book = programmingBook;
+console.log(book);
+
+// 초과 프로퍼티 검사
+let book2: Book = {
+  name: "한 입 크기로 잘라먹는 리액트",
+  price: 33000,
+  // skill: "reactjs",
+};
+
+let book3: Book = programmingBook;
